e4af5ca12e7609767c926050915ddafa52ad9942
Ansell Gabriel Del Ayre <ansellgabriel@gmail.com>
2023-11-16 08:25:46 +0800

    update main

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -25,4 +25,13 @@ class Batch < ApplicationRecord
     return dc_prm
   end
 
+  def self.to_csv 
+    CSV.generate(col_sep: ";") do |csv|
+      csv << ["id", "coop","last_name", "first_name", "middle_name", "birth_date", "mobile_no", "email", "guest_type", "attend", "price", "paid", "award", "size", "tentative", "dietary", "registered"]
+      all.find_each do |reg|
+        csv << [reg.id, reg.event_hub.cooperative.name, reg.last_name, reg.first_name, reg.middle_name, reg.birth_date, reg.mobile_number, reg.email, reg.guest_type, reg.attend, reg.price, reg.paid, reg.award, reg.size, reg.tentative, reg.dietary, reg.created_at]
+      end
+    end
+  end
+
 end
