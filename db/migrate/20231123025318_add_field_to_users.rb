3a30a85c15cf04099c64f0693ebb7787abf11b94
snjhyrglr <jayjregalario@gmail.com>
2023-11-29 16:36:56 +0800

    fix duplicate column approved

--- db/migrate/20231123025318_add_field_to_users.rb
+++ db/migrate/20231123025318_add_field_to_users.rb
@@ -1,5 +1,5 @@
 class AddFieldToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :approved, :boolean, default: false
+    # add_column :users, :approved, :boolean, default: false
   end
 end
